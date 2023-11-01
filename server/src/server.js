7a25ed5292235d1c310ca5b1f783c0393af90ec7
tejas-salespoint <tejas.lade@salespintinc.com>
2023-11-01 23:28:38 +0530

    mongodb connection successfull

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,30 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+    "mongodb+srv://tejaslade:$tejaslade$@cluster0.bonpjls.mongodb.net/custom_datagrid?retryWrites=true&w=majority";
 
-const server = http.createServer(app)
 
-server.listen(PORT,() => {
-    console.log(`Listening on port ${PORT}`);
+
+const server = http.createServer(app);
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB is Connection ready');
 })
 
+mongoose.connection.on('error', (err) => {
+    console.log(err)
+})
+
+async function startServer() {
+    await mongoose.connect(MONGO_URL);
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    })
+
+}
+
+
+startServer();
